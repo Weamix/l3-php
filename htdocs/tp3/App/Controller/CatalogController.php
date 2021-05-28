939668614f6dd97c63d44f2a8fc4f90043e77509
Vitse Maxime <maxime.vitse@odaxiom.com>
2021-05-28 17:42:27 +0200

    TP3 Router WIP

--- htdocs/tp3/App/Controller/CatalogController.php
+++ htdocs/tp3/App/Controller/CatalogController.php
@@ -7,6 +7,11 @@ namespace App\Controller;
 class CatalogController
 {
     public function view(){
-        return "Hello view catalog";
+        return "view catalog";
     }
+
+    public function viewProduct(){
+        return "view product";
+    }
+
 }
--- htdocs/tp3/Router.php
+++ htdocs/tp3/Router.php
@@ -2,18 +2,19 @@
 
 class Router{
     function process(){
-        /* ex http://localhost/
-        $uri = /
+        /*
+            ex http://localhost/
+            $uri = /
 
-        ex http://localhost/catalog
-        $uri = /catalog
+            ex http://localhost/catalog
+            $uri = /catalog
 
-        ex http://localhost/catalog
-        $uri = /catalog/product
+            ex http://localhost/catalog
+            $uri = /catalog/product
 
-        1) récup uri
-        2) mapping entre $uri et routes.json : prévoir route non connue => 404
-        3) instance controller de la route avec appel de la méthode
+            1) récup uri
+            2) mapping entre $uri et routes.json : prévoir route non connue => 404
+            3) instance controller de la route avec appel de la méthode
         */
 
         $uri = $_SERVER['REQUEST_URI'];
@@ -21,10 +22,20 @@ class Router{
 
         $json = file_get_contents("routes.json");
         $routes = json_decode($json, true);
+        $controller=null;
 
         foreach ($routes as $route){
             echo $route['path']."<br>";
-        }
+                if($uri == $route['path']){
+                    $controller = $route['controller'];
+                } else{
+                    echo "404";
+                    header("HTTP/1.0 404 Not Found");
+                    http_response_code(404);
+                }
+            }
 
-    }
+            list($controllerObject, $method) = explode("@", $controller);
+            return call_user_func_array([$controllerObject, $method], []);
+        }
 }
