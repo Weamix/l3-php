3dfdc55b122199029a5f2e973515705d03a4595d
Vitse Maxime <maxime.vitse@odaxiom.com>
2021-06-18 11:21:36 +0200

    Update CustomerRepo and View

---
+++ htdocs/App/Controller/CustomerController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+
+class CatalogController extends AbstractController
+{
+
+    public function view()
+    {
+        $list_product = [];
+        $list_product_by_id= [];
+
+        $productRepo = new \App\Entity\Repository\Product();
+        $list_product = $productRepo->findAll();
+
+        $list_product_by_id = $productRepo->findBy('price',100);
+
+        $this->render('catalogue/view.phtml', ['products' => $list_product , 'products_by_id' => $list_product_by_id]);
+    }
+
+    public function viewProduct(){
+        $productRepo = new \App\Entity\Repository\Product();
+        $product = $productRepo->find(1);
+        $this->render("catalogue/viewProduct.phtml", ['product' => $product]);
+        //echo "view product";
+    }
+}
