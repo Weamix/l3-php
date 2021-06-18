77e836852f309ebc530949f10ad08e888ccda0b0
Vitse Maxime <maxime.vitse@odaxiom.com>
2021-06-18 11:46:10 +0200

    Add ToDo with call Api

--- htdocs/App/Controller/ToDoController.php
+++ htdocs/App/Controller/ToDoController.php
@@ -9,8 +9,12 @@ class TodoController extends AbstractController
 
     public function home()
     {
-        // Use guzzle call api todo
-        // https://jsonplaceholder.typicode.com/todos
+        // Use guzzle call api todo :https://jsonplaceholder.typicode.com/todos
+        $client = new Client();
+        $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/todos');
+
+        $todos = json_decode($response->getBody()->getContents());
+        $this->render("todos/viewTodos.phtml", ["todos" => $todos]);
     }
 
 }
