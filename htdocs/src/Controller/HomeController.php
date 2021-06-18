86d70cd13cf75d04719864026721932fb7d8e170
Vitse Maxime <maxime.vitse@odaxiom.com>
2021-06-18 15:25:36 +0200

    home controller

---
+++ htdocs/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+
+        return new Response(
+            '<html><body>Home page</body></html>'
+        );
+    }
+}
